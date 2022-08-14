5b076362594389283efd87c354ee0bd74c4efeda
darrida <darrida.py@gmail.com>
2022-08-14 17:05:39 -0500

    Migrated to Prefect 2; migrated from boto3 to prefect-aws

--- src/flow.py
+++ src/flow.py
@@ -1,15 +1,34 @@
-from prefect import flow
+from prefect import flow, get_run_logger
 from prefect.task_runners import SequentialTaskRunner
-from src.tasks import aws_get_to_calc, calc_yearly_avg
+from prefect.filesystems import S3
+from prefect_aws import AwsCredentials
+from prefect_aws.s3 import s3_list_objects, s3_download, s3_upload
+from src.tasks import calc_yearly_avg
+
+
+s3_block = S3.load("noaa-data")
 
 
 @flow(name="NOAA-1-yearly-average", task_runner=SequentialTaskRunner())
 def main():
+    logger = get_run_logger()
+    
     region = "us-east-1"
     bucket = "noaa-temperature-data"
+    aws_credentials = AwsCredentials(aws_access_key_id=s3_block.aws_access_key_id.get_secret_value(), 
+                                     aws_secret_access_key=s3_block.aws_secret_access_key.get_secret_value())
 
-    files_l = aws_get_to_calc(bucket, region, prefix='data')
-    calc_yearly_avg(bucket, region, files_l)
+    s3_objects = s3_list_objects(bucket=bucket, prefix="data", aws_credentials=aws_credentials)
+    files_l = [x["Key"] for x in s3_objects]
+    files_l = [x for x in files_l if '_full.csv' in x]
+
+    for filename in files_l:
+        print(filename)
+        year_obj = s3_download(bucket=bucket, key=filename, aws_credentials=aws_credentials)
+        avg_obj = calc_yearly_avg(year_obj, filename)
+        key = s3_upload(bucket=bucket, key=f"year_average/{filename.split('/')[1][:4]}_averages.csv", # example "filename": "data/YYYY_full.csv"
+                        data=avg_obj, aws_credentials=aws_credentials)
+        logger.info(f"Stored as '{key}'")
 
 
 if __name__ == "__main__":
--- src/support.py
+++ src/support.py
@@ -1,30 +1,11 @@
-import boto3
 import pandas as pd
 
 
-def initialize_s3_client(region_name: str) -> boto3.client:
-    return boto3.client("s3", region_name=region_name)
-
-
-def aws_all_data_files(s3_client, bucket, prefix):
-    paginator = s3_client.get_paginator("list_objects_v2")
-    pages = paginator.paginate(Bucket=bucket, Prefix=prefix)
-    files_l = []
-    for page in pages:
-        list_all_keys = page["Contents"]
-        # item arrives in format of 'year/filename'; this removes 'year/'
-        temp_l = [x["Key"].split("/")[1] for x in list_all_keys]
-        files_l += temp_l
-    return files_l
-
-
-# @task()
 def set_station_as_index(df: pd.DataFrame) -> pd.DataFrame:
     df[['LATITUDE', 'LONGITUDE', 'ELEVATION']] = df[['LATITUDE', 'LONGITUDE', 'ELEVATION']].fillna('missing')
     return df.set_index('STATION')
 
 
-# @task()
 def remove_missing_spatial(df: pd.DataFrame) -> pd.DataFrame:
     """
     Removes Records with Missing Spatial Data
